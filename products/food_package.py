99b25dfef9267f3fb37eda8b789d1b45a4c13c2c
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-06-16 16:51:29 +0200

    pep8

--- products/food_package.py
+++ products/food_package.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class FoodPackage (ABC): 
     @abstractmethod
     def pack(self)  -> str:
--- products/product.py
+++ products/product.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-#Write your code here
+
 
 class Product(ABC):
     def __init__(self, id:str, name:str, price:float):
@@ -13,25 +13,32 @@ class Product(ABC):
     @abstractmethod
     def type(self) -> str:
         pass
+
     @abstractmethod
     def foodPackage(self)->FoodPackage:
         pass  
 
+
 class Hamburger(Product):
     def __init__(self, id:str, name:str, price:float):
         super().__init__(id, name, price)
+
     def type(self) -> str:
         return "Hamburguesa"
+
     def foodPackage(self) -> FoodPackage:
         return Wrapping()
-        
+
+
 class Soda(Product):
     #Write your code here
     pass
 
+
 class Drink(Product):
     #Write your code here
     pass
+
 
 class HappyMeal(Product):
     #Write your code here
