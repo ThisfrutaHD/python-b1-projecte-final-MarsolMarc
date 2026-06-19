35c22317637f629787be02f3dced36adad2baed7
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-06-19 04:00:26 +0200

    limitem maxim de columnes

--- prepare_order.py
+++ prepare_order.py
@@ -1,19 +1,24 @@
 """
 Ejercicio 1: Sistema de comida rápida
  
-Implementar un paquete llamado ‘products' que tiene dos módulos: ‘food_package.py' y ‘product.py', con la siguiente estructura:
+Implementar un paquete llamado ‘products' que tiene dos módulos: ‘food_package.py' y ‘product.py', con la siguiente 
+estructura:
 
 products/
         __init__.py
         food_package.py
         product.py
 
-El módulo food_package.py contendrá una clase abstracta denominada 'FoodPackage' con dos funciones abstractas: 'def pack(self)  -> str ' y 'def material(self) -> str '. Esta clase nos permite crear un tipo específico de paquete o envoltura dependiendo del tipo de alimento a empacar, por ejemplo:
+El módulo food_package.py contendrá una clase abstracta denominada 'FoodPackage' con dos funciones abstractas: 
+'def pack(self)  -> str ' y 'def material(self) -> str '. Esta clase nos permite crear un tipo específico de paquete o 
+envoltura dependiendo del tipo de alimento a empacar, por ejemplo:
 
 Un vaso de soda puede ser empacado en un paquete tipo vaso y el material puede ser cartón. 
 Una hamburguesa puede ser empacada en un paquete tipo envoltura de papel y el material puede ser aluminio.
 
-En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las siguientes clases ‘Wrapping’, ‘Bottle’, ‘Glass’ y ‘Box’, es decir, estas deben implementar los métodos anteriores y devolver un valor. Por ejemplo, la clase ‘Wrapping’ se puede definir como:
+En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las siguientes clases ‘Wrapping’, 
+‘Bottle’, ‘Glass’ y ‘Box’, es decir, estas deben implementar los métodos anteriores y devolver un valor. Por ejemplo, 
+la clase ‘Wrapping’ se puede definir como:
 
 class Wrapping(FoodPackage):  
   def pack(self):
@@ -21,12 +26,18 @@ class Wrapping(FoodPackage):
   def material(self):
     return "Aluminium" 
 
-El módulo 'product.py’ contendrá una clase abstracta denominada 'Product' con dos funciones abstractas: 'def type(self) -> str' y 'def foodPackage(self)-> FoodPackage. Esta clase nos permita crear un producto específico y relacionarlo con su tipo de empaque por ejemplo:
+El módulo 'product.py’ contendrá una clase abstracta denominada 'Product' con dos funciones abstractas:
+ 'def type(self) -> str' y 'def foodPackage(self)-> FoodPackage. Esta clase nos permita crear un producto específico y 
+ relacionarlo con su tipo de empaque por ejemplo:
 
-Un producto con código de barras G1, es una soda Sprite cuyo precio es de 5 euros, pertenece al tipo Soda y puede ser empacado en un paquete tipo vaso y el material puede ser cartón. 
-Un producto con código de barras H1, es una hamburguesa Bacon  cuyo precio es de 15 euros, pertenece al tipo Hamburger y puede ser empacado en un paquete un paquete tipo envoltura de papel y el material puede ser aluminio.
+Un producto con código de barras G1, es una soda Sprite cuyo precio es de 5 euros, pertenece al tipo Soda y puede ser 
+empacado en un paquete tipo vaso y el material puede ser cartón. 
+Un producto con código de barras H1, es una hamburguesa Bacon  cuyo precio es de 15 euros, pertenece al tipo Hamburger 
+y puede ser empacado en un paquete un paquete tipo envoltura de papel y el material puede ser aluminio.
 
-En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases ‘Hamburger’, ‘Soda’, ‘Drink’ y ‘HappyMeal’, es decir, de forma parecida al módulo anterior, estas deben implementar los métodos anteriores y devolver un valor. Por ejemplo, la clase ‘Hamburger’, se puede definir como:
+En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases
+ ‘Hamburger’, ‘Soda’, ‘Drink’ y ‘HappyMeal’, es decir, de forma parecida al módulo anterior, estas deben implementar los 
+ métodos anteriores y devolver un valor. Por ejemplo, la clase ‘Hamburger’, se puede definir como:
 
 class Hamburger(Product):
     def __init__(self, id:str, name:str, price:float):
@@ -42,21 +53,29 @@ users/
         __init__.py
         user.py
 
-El módulo 'user.py' contendrá una clase abstracta denominada ‘User’ que tiene un constructor por defecto para los siguientes datos 'def __init__(self, dni:str, name:str, age:int) ', con una función abstracta: 'def describe(self) '.
+El módulo 'user.py' contendrá una clase abstracta denominada ‘User’ que tiene un constructor por defecto para los 
+siguientes datos 'def __init__(self, dni:str, name:str, age:int) ', con una función abstracta: 'def describe(self) '.
 
-Luego en el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases ‘Cashier’ y ‘Customer’, es decir, estas deben implementar los métodos anteriores y devolver un valor. Adicionalmente, estas clases se diferencian por los parámetros que reciben sus constructores, por tanto, debemos hacer uso de herencia para inicializar el constructor de la clase padre y agregar características propias a cada clase.  
+Luego en el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases
+ ‘Cashier’ y ‘Customer’, es decir, estas deben implementar los métodos anteriores y devolver un valor. 
+ Adicionalmente, estas clases se diferencian por los parámetros que reciben sus constructores, por tanto,
+   debemos hacer uso de herencia para inicializar el constructor de la clase padre y agregar características 
+   propias a cada clase.  
 
-Implementar un paquete llamado 'util' que tiene dos módulos, denominados 'file_manager.py' y 'converter.py’, con la siguiente estructura:
+Implementar un paquete llamado 'util' que tiene dos módulos, denominados 'file_manager.py' y 'converter.py’, 
+con la siguiente estructura:
 
 util/
         __init__.py
         file_manager.py
         converter.py
 
-El módulo ‘file_manager.py' contendrá una clase ‘CSVFileManager’ la cual es una implementaciòn libre y debe incluir las funciones:
+El módulo ‘file_manager.py' contendrá una clase ‘CSVFileManager’ la cual es una implementaciòn libre y debe incluir 
+las funciones:
 
 La función 'def read(self)' lee un archivo en formato CSV y permite exportar su resultado como un Data Frame.
-La función 'def write(self, dataFrame)' convierte un Data Frame en un archivo CSV. Esta es una función opcional, se deja al estudiante la implementación.
+La función 'def write(self, dataFrame)' convierte un Data Frame en un archivo CSV. Esta es una función opcional, 
+se deja al estudiante la implementación.
 
 Los archivos en formato CSV se encuentran en la ruta “data/”, a continuación, se describe el contenido de cada archivo:
 
@@ -67,7 +86,11 @@ sodas.csv: Información de los diferentes tipos de gaseosas.
 hamburgers.csv: Información de los diferentes tipos de hamburguesas.
 happyMeal.csv: Información de los diferentes tipos de happy meals.
 
-El módulo 'converter.py' contendrá una clase denominada ‘Converter’ con una función abstracta para convertir las filas de un Data Frame en instancias de objetos. La función sería ‘def convert(self, dataFrame, *args) -> list’. Adicionalmente esta clase debe incluir un método que permite imprimir la información de los objetos ‘def print(self, list)’. En el mismo módulo se deberán incluir las implementaciones específicas que permitan leer los archivos en formato CSV y convertir sus filas en objetos de cada clase utilizando los paquetes product y users.
+El módulo 'converter.py' contendrá una clase denominada ‘Converter’ con una función abstracta para convertir 
+las filas de un Data Frame en instancias de objetos. La función sería ‘def convert(self, dataFrame, *args) -> list’. 
+Adicionalmente esta clase debe incluir un método que permite imprimir la información de los objetos ‘def print(self, list)’. 
+En el mismo módulo se deberán incluir las implementaciones específicas que permitan leer los archivos en formato CSV y 
+convertir sus filas en objetos de cada clase utilizando los paquetes product y users.
 
 Implementar un paquete llamado 'orders' que tiene un módulo 'order.py', con la siguiente estructura:
 
@@ -75,9 +98,17 @@ orders/
         __init__.py
         order.py
 
-El módulo 'order.py' contendrá una clase denominada ‘Order’ con un constructor ‘def __init__(self, cashier:Cashier, customer:Customer):’, el cual permite inicializar la clase con los datos del cajero, del cliente y la lista de productos vacía por defecto. Además, debe incluir tres funciones para agregar productos, calcular el total de la orden solicitada y mostrar la información de la orden que está siendo procesada. Las funciones son ‘def add(self, product: Product)', ' def calculateTotal(self) -> float' y ‘def show(self)’, respectivamente.
+El módulo 'order.py' contendrá una clase denominada ‘Order’ con un constructor ‘def __init__(self, cashier:Cashier, 
+customer:Customer):’, el cual permite inicializar la clase con los datos del cajero, del cliente y la lista de productos 
+vacía por defecto. Además, debe incluir tres funciones para agregar productos, calcular el total de la orden solicitada 
+y mostrar la información de la orden que está siendo procesada. Las funciones son ‘def add(self, product: Product)', ' 
+def calculateTotal(self) -> float' y ‘def show(self)’, respectivamente.
 
-Finalmente tendremos una clase principal que se llamará ‘PrepareOrder’ en la cual se deberá realizar una implementación que permita integrar los diferentes módulos empleados para leer los archivos en formato CSV y convertirlos en objetos. La implementación de esta clase es libre, es decir, no indicaremos las funciones que debe contener, pero la funcionalidad de la clase debe permitir crear una opción de menú que permita buscar los clientes, los cajeros y los productos para finalmente crear una orden. 
+Finalmente tendremos una clase principal que se llamará ‘PrepareOrder’ en la cual se deberá realizar una implementación 
+que permita integrar los diferentes módulos empleados para leer los archivos en formato CSV y convertirlos en objetos. 
+La implementación de esta clase es libre, es decir, no indicaremos las funciones que debe contener, pero la funcionalidad 
+de la clase debe permitir crear una opción de menú que permita buscar los clientes, los cajeros y los productos para 
+finalmente crear una orden. 
 
 Se sugiere utilizar los métodos de entrada de teclado para leer los datos del dni cajero, cliente e id de los productos. 
 
